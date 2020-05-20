99d65d6ceda9ca53525f95d22caf621eb0448547
Guven <guven89@hotmail.com>
2020-05-21 01:47:35 +0200

    Add project files.

---
+++ ProjectInsights/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace ProjectInsights
+{
+    partial class FormInsights
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShow = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSimilarity = new System.Windows.Forms.Label();
+            this.txtSimilarity = new System.Windows.Forms.TextBox();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(12, 117);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(100, 35);
+            this.btnShow.TabIndex = 0;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Project Insights";
+            // 
+            // lblSimilarity
+            // 
+            this.lblSimilarity.AutoSize = true;
+            this.lblSimilarity.Location = new System.Drawing.Point(12, 57);
+            this.lblSimilarity.Name = "lblSimilarity";
+            this.lblSimilarity.Size = new System.Drawing.Size(83, 17);
+            this.lblSimilarity.TabIndex = 2;
+            this.lblSimilarity.Text = "Similarity (%)";
+            // 
+            // txtSimilarity
+            // 
+            this.txtSimilarity.Location = new System.Drawing.Point(12, 77);
+            this.txtSimilarity.Name = "txtSimilarity";
+            this.txtSimilarity.Size = new System.Drawing.Size(100, 25);
+            this.txtSimilarity.TabIndex = 3;
+            this.txtSimilarity.Text = "65";
+            // 
+            // txtContent
+            // 
+            this.txtContent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtContent.Location = new System.Drawing.Point(148, 77);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtContent.Size = new System.Drawing.Size(774, 361);
+            this.txtContent.TabIndex = 4;
+            // 
+            // FormInsights
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 450);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.txtSimilarity);
+            this.Controls.Add(this.lblSimilarity);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnShow);
+            this.Name = "FormInsights";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Project Insights";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSimilarity;
+        private System.Windows.Forms.TextBox txtSimilarity;
+        private System.Windows.Forms.TextBox txtContent;
+    }
+}
+
---
+++ ProjectInsights/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProjectInsights
+{
+    public partial class FormInsights : Form
+    {
+        public FormInsights()
+        {
+            InitializeComponent();
+        }
+        private const string directory = @"C:\Users\Guven\Desktop\BI";
+        private void btnShow_Click(object sender, System.EventArgs e)
+        {
+            var sb = new StringBuilder();
+            ProcessStartInfo startInfo = new ProcessStartInfo()
+            {
+                FileName = "git",
+                WorkingDirectory = directory,
+                CreateNoWindow = true,
+                Arguments = @"--git-dir C:\Users\Guven\Desktop\BI\.git ls-files",
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+
+            Process cmdProcess = Process.Start(startInfo);
+            string line;
+            int count = 0;
+            while ((line = cmdProcess.StandardOutput.ReadLine()) != null)
+            {
+                sb.Append("\r\n" + line);
+                count++;
+            }
+            txtContent.Text += sb;
+
+            txtContent.Text += "\r\n\r\nFiles Count: " + count;
+        }
+    }
+}
