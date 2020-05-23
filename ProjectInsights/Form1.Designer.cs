295ce431ccae97b002aa7496965eea27e0927d4f
Guven <guven89@hotmail.com>
2020-05-23 16:19:24 +0200

    Get results from git blame filter & sanitize & combine similar names and show

--- ProjectInsights/Form1.Designer.cs
+++ ProjectInsights/Form1.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class FormInsights
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,26 +13,23 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.btnShow = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.lblSimilarity = new System.Windows.Forms.Label();
-            this.txtSimilarity = new System.Windows.Forms.TextBox();
             this.txtContent = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtProjectPath = new System.Windows.Forms.TextBox();
+            this.lblError = new System.Windows.Forms.Label();
+            this.txtSimilarity = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnShow
             // 
             this.btnShow.Location = new System.Drawing.Point(12, 117);
             this.btnShow.Name = "btnShow";
-            this.btnShow.Size = new System.Drawing.Size(100, 35);
+            this.btnShow.Size = new System.Drawing.Size(130, 35);
             this.btnShow.TabIndex = 0;
             this.btnShow.Text = "Show";
             this.btnShow.UseVisualStyleBackColor = true;
@@ -64,51 +54,81 @@
             this.lblSimilarity.TabIndex = 2;
             this.lblSimilarity.Text = "Similarity (%)";
             // 
-            // txtSimilarity
-            // 
-            this.txtSimilarity.Location = new System.Drawing.Point(12, 77);
-            this.txtSimilarity.Name = "txtSimilarity";
-            this.txtSimilarity.Size = new System.Drawing.Size(100, 25);
-            this.txtSimilarity.TabIndex = 3;
-            this.txtSimilarity.Text = "65";
-            // 
             // txtContent
             // 
             this.txtContent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtContent.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.txtContent.Location = new System.Drawing.Point(148, 77);
             this.txtContent.Multiline = true;
             this.txtContent.Name = "txtContent";
             this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtContent.Size = new System.Drawing.Size(774, 361);
+            this.txtContent.Size = new System.Drawing.Size(456, 382);
             this.txtContent.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Project path (.git)";
+            // 
+            // txtProjectPath
+            // 
+            this.txtProjectPath.Location = new System.Drawing.Point(262, 9);
+            this.txtProjectPath.Name = "txtProjectPath";
+            this.txtProjectPath.Size = new System.Drawing.Size(342, 25);
+            this.txtProjectPath.TabIndex = 6;
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Location = new System.Drawing.Point(610, 12);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 17);
+            this.lblError.TabIndex = 7;
+            // 
+            // txtSimilarity
+            // 
+            this.txtSimilarity.Location = new System.Drawing.Point(13, 78);
+            this.txtSimilarity.Name = "txtSimilarity";
+            this.txtSimilarity.Size = new System.Drawing.Size(129, 25);
+            this.txtSimilarity.TabIndex = 8;
+            this.txtSimilarity.Text = "70";
+            this.txtSimilarity.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSimilarity_KeyPress);
             // 
             // FormInsights
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(934, 450);
-            this.Controls.Add(this.txtContent);
+            this.ClientSize = new System.Drawing.Size(615, 472);
             this.Controls.Add(this.txtSimilarity);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.txtProjectPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtContent);
             this.Controls.Add(this.lblSimilarity);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnShow);
             this.Name = "FormInsights";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Project Insights";
+            this.Load += new System.EventHandler(this.FormInsights_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
-        #endregion
-
         private System.Windows.Forms.Button btnShow;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblSimilarity;
-        private System.Windows.Forms.TextBox txtSimilarity;
         private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtProjectPath;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.TextBox txtSimilarity;
     }
 }
-
--- ProjectInsights/Form1.cs
+++ ProjectInsights/Form1.cs
@@ -1,4 +1,8 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
@@ -10,31 +14,219 @@ namespace ProjectInsights
         {
             InitializeComponent();
         }
-        private const string directory = @"C:\Users\Guven\Desktop\BI";
-        private void btnShow_Click(object sender, System.EventArgs e)
+
+        private const string CSharpExtention = ".cs";
+        private readonly int stopCount = 15;
+        private readonly HashSet<string> excludedDirectories = new HashSet<string>() { "Proxies", "Migrations", };
+        //private readonly char[] nameDelimeters = new[] { ' ', '.', '-' };
+
+        private const string projectDirectory = @"C:\Users\Guven\Desktop\BI";
+        private const string space = " ";
+
+        private void btnShow_Click(object sender, EventArgs e)
         {
-            var sb = new StringBuilder();
+            try
+            {
+                txtContent.Text = string.Empty;
+
+                Process gitProcess = CreateGitProcess("ls-files");
+                HashSet<string> files = GetFiles(gitProcess.StandardOutput);
+                var metrics = GetMetrics(files);
+                ShowMetrics(metrics);
+                //ShowFiles(files);
+
+            }
+            catch (Exception ex) { lblError.Text = ex.Message; }
+        }
+
+        private Dictionary<string, int> GetMetrics(HashSet<string> files)
+        {
+            var metrics = CalculateMetrics(files);
+
+            EliminateDuplicates(metrics);
+
+            var sortedMetrics = metrics.OrderByDescending(a => a.Value).ToDictionary(a => a.Key, b => b.Value);
+
+            return sortedMetrics;
+        }
+
+        private void EliminateDuplicates(Dictionary<string, int> metricsDictionary)
+        {
+            var dic = new Dictionary<string, int>();
+            bool combinationFound = true;
+
+            while (combinationFound)
+            {
+                combinationFound = false;
+                foreach (var metric in metricsDictionary)
+                {
+                    string firstName = metric.Key.Split(space).First();
+                    foreach (var otherKey in metricsDictionary.Keys.Where(a => a != metric.Key))
+                    {
+                        string otherFirstName = otherKey.Split(space).First();
+                        int similarityAllowance = int.Parse(txtSimilarity.Text);
+
+                        if (StringHelper.GetSimilarityPercentage(otherFirstName, firstName) >= similarityAllowance)
+                        {
+                            int total = metricsDictionary[metric.Key] + metricsDictionary[otherKey];
+
+                            if (metric.Key.Length > otherKey.Length)
+                            {
+                                metricsDictionary[metric.Key] = total;
+                                metricsDictionary.Remove(otherKey);
+                            }
+                            else
+                            {
+                                metricsDictionary[otherKey] = total;
+                                metricsDictionary.Remove(metric.Key);
+                            }
+                            combinationFound = true;
+                            break;
+                        }
+                    }
+                    if (combinationFound) break;
+                }
+            }
+        }
+
+        private Dictionary<string, int> CalculateMetrics(HashSet<string> files)
+        {
+            var metrics = new Dictionary<string, int>();
+            foreach (var file in files)
+            {
+                var fileMetrics = GetFileMetrics(file);
+                AddFileMetrics(metrics, fileMetrics);
+            }
+
+            return metrics;
+        }
+
+        private static void PrintOutput()
+        {
+            var sw = new StreamWriter(@"C:\Users\Guven\Desktop\out.txt");
+            foreach (var item in output)
+                sw.WriteLine(item);
+            sw.Close();
+        }
+
+        private Dictionary<string, int> GetFileMetrics(string fileName)
+        {
+            string gitBlameCommand = $"blame {fileName} -fte";
+            var gitProcess = CreateGitProcess(gitBlameCommand);
+            var authors = GetAuthorsFromFile(gitProcess.StandardOutput, fileName);
+            var fileMetrics = authors.GroupBy(a => a)
+                .ToDictionary(g => g.Key, g => g.Count());
+
+            return fileMetrics;
+        }
+
+        private static void AddFileMetrics(Dictionary<string, int> metrics, Dictionary<string, int> fileMetrics)
+        {
+            foreach (var pair in fileMetrics)
+            {
+                if (!metrics.ContainsKey(pair.Key))
+                    metrics[pair.Key] = pair.Value;
+                else
+                    metrics[pair.Key] += pair.Value;
+            }
+        }
+        static readonly List<string> output = new List<string>();
+
+        private List<string> GetAuthorsFromFile(StreamReader output, string fileName)
+        {
+            var list = new List<string>();
+
+            string line;
+            while ((line = output.ReadLine()) != null)
+            {
+                if (line.Contains(fileName))
+                {
+                    string author = SanitizeLine(line);
+                    list.Add(author);
+                    //if (author.ToLower().Contains("somename"))
+                    //    ben.Add(fileName);
+                }
+            }
+
+            return list;
+        }
+
+        private static string SanitizeLine(string line)
+        {
+            int startIdx = line.IndexOf("<") + 1;
+            int length = line.IndexOf(">") - startIdx;
+            line = line.Substring(startIdx, length);
+            line = line.Substring(0, line.IndexOf("@"));
+            line = line.Replace(".", space);
+            return line;
+        }
+
+        private Process CreateGitProcess(string command)
+        {
+            string prjAbsolutePath = txtProjectPath.Text;
             ProcessStartInfo startInfo = new ProcessStartInfo()
             {
                 FileName = "git",
-                WorkingDirectory = directory,
+                WorkingDirectory = prjAbsolutePath,
                 CreateNoWindow = true,
-                Arguments = @"--git-dir C:\Users\Guven\Desktop\BI\.git ls-files",
+                Arguments = $@"--git-dir {prjAbsolutePath}\.git {command}",
                 RedirectStandardOutput = true,
                 UseShellExecute = false
             };
 
             Process cmdProcess = Process.Start(startInfo);
+            return cmdProcess;
+        }
+
+        private HashSet<string> GetFiles(StreamReader output)
+        {
+            var files = new HashSet<string>();
+            int count = 1;
+
             string line;
-            int count = 0;
-            while ((line = cmdProcess.StandardOutput.ReadLine()) != null)
+            while ((line = output.ReadLine()) != null)
             {
-                sb.Append("\r\n" + line);
-                count++;
+                if (count == stopCount && stopCount != -1) break;
+
+                string extension = Path.GetExtension(line);
+                bool includedDirectory = excludedDirectories.Where(a => line.Contains(a)).Count() == 0;
+                if (extension == CSharpExtention && includedDirectory)
+                {
+                    files.Add(line);
+                    count++;
+                }
+            }
+            return files;
+        }
+
+        private void ShowFiles(HashSet<string> files)
+        {
+            txtContent.Text += string.Join(Environment.NewLine, files);
+            txtContent.Text += Environment.NewLine + Environment.NewLine;
+            txtContent.Text += $"Files Count: {files.Count}";
+        }
+
+        private void ShowMetrics(Dictionary<string, int> metrics)
+        {
+            var sb = new StringBuilder();
+            foreach (var metric in metrics)
+            {
+                string formatted = string.Format("{0,-20}\t : {1,-6}", metric.Key, metric.Value);
+                sb.AppendLine(formatted);
             }
             txtContent.Text += sb;
+        }
 
-            txtContent.Text += "\r\n\r\nFiles Count: " + count;
+        private void FormInsights_Load(object sender, EventArgs e)
+        {
+            txtProjectPath.Text = projectDirectory;
+        }
+
+        private void txtSimilarity_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!char.IsDigit(ch) && ch != 8)
+                e.Handled = true;
         }
     }
 }
